3266188699cd890df192984dcbd8ab795bae6fb2
skyclouds2001 <skyclouds2001@163.com>
2025-07-20 13:26:28 +0800

    更新项目配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,14 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 
 export default [{
     files: ["**/*.ts"],
 }, {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+        "@typescript-eslint": tseslint.plugin,
     },
 
     languageOptions: {
-        parser: tsParser,
+        parser: tseslint.parser,
         ecmaVersion: 2022,
         sourceType: "module",
     },
