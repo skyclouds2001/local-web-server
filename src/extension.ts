50e8d1d1f5088623f9d1226f7a6f87a741a2a080
skyclouds2001 <skyclouds2001@163.com>
2025-07-27 01:26:46 +0800

    为侧边栏添加webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,11 @@
 import * as vscode from 'vscode';
+import fs from 'node:fs';
 
+const extensionName = 'Local Web Server';
 const startServerCommand = 'local-web-server.startServer';
 const stopServerCommand = 'local-web-server.stopServer';
 const toggleServerCommand = 'local-web-server.toggleServer';
+const configurationViewId = 'local-web-server-configuration';
 
 let hasActiveServer = false;
 let statusBarItem: vscode.StatusBarItem | null = null;
@@ -22,12 +25,36 @@ export function activate(context: vscode.ExtensionContext) {
 
   const statusBarAlignment = vscode.window.activeTextEditor ? vscode.StatusBarAlignment.Right : vscode.StatusBarAlignment.Left;
   statusBarItem = vscode.window.createStatusBarItem('', statusBarAlignment, 99);
-  statusBarItem.name = 'Local Web Server';
+  statusBarItem.name = extensionName;
   statusBarItem.text = '$(server) Local Web Server';
   statusBarItem.tooltip = 'Click to start the Local Web Server';
   statusBarItem.command = toggleServerCommand;
   statusBarItem.show();
   context.subscriptions.push(statusBarItem);
+
+  const sidebarUri = vscode.Uri.joinPath(context.extensionUri, 'static', 'sidebar.html');
+  const sidebarHtmlContent = fs.readFileSync(sidebarUri.fsPath, 'utf-8');
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(configurationViewId, new class implements vscode.WebviewViewProvider {
+      constructor (private readonly extensionUri: vscode.Uri, private readonly extensionName: string) {}
+
+      public resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext, token: vscode.CancellationToken)  {
+        webviewView.webview.options = {
+          enableScripts: true,
+          localResourceRoots: [
+            this.extensionUri,
+          ],
+        };
+        webviewView.webview.html = sidebarHtmlContent
+          .replaceAll('%title%', extensionName)
+          .replaceAll('%csp%', webviewView.webview.cspSource);
+      }
+    }(context.extensionUri, extensionName), {
+      webviewOptions: {
+        retainContextWhenHidden: true,
+      }
+    })
+  );
 }
 
 export function deactivate() {}
